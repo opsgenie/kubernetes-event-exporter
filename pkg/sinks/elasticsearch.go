70fe18dd1838f2acfa74eb3840958a4232fe65bd
Ubuntu <ubuntu@ip-10-162-138-41.us-west-2.compute.internal>
2020-07-11 02:27:05 +0000

    rev-es

--- pkg/sinks/elasticsearch.go
+++ pkg/sinks/elasticsearch.go
@@ -1,137 +1,145 @@
 package sinks
 
 import (
-	// "bytes"
-	"bufio"
-	"cloud.google.com/go/bigquery"
+	"bytes"
 	"context"
-	//"encoding/json"
-	"os"
-	// "crypto/tls"
-	// "encoding/json"
-	"fmt"
-	//"github.com/elastic/go-elasticsearch/v7"
-	// "github.com/elastic/go-elasticsearch/v7/esapi"
-	"github.com/opsgenie/kubernetes-event-exporter/pkg/batch"
+	"crypto/tls"
+	"encoding/json"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
 	"github.com/opsgenie/kubernetes-event-exporter/pkg/kube"
-	"github.com/rs/zerolog/log"
-	// "io/ioutil"
-	// "net/http"
-	//"regexp"
-	//"strings"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
 	"time"
 )
 
-func writeBatchToJsonFile(items []interface{}, path string) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
-	for i := 0; i < len(items); i++ {
-                event := items[i].(*kube.EnhancedEvent)
-		fmt.Fprintln(writer, string(event.ToJSON()))
-	}
-	return writer.Flush()
+type ElasticsearchConfig struct {
+	// Connection specific
+	Hosts    []string `yaml:"hosts"`
+	Username string   `yaml:"username"`
+	Password string   `yaml:"password"`
+	CloudID  string   `yaml:"cloudID"`
+	APIKey   string   `yaml:"apiKey"`
+	// Indexing preferences
+	UseEventID  bool   `yaml:"useEventID"`
+	Index       string `yaml:"index"`
+	IndexFormat string `yaml:"indexFormat"`
+	TLS         struct {
+		InsecureSkipVerify bool   `yaml:"insecureSkipVerify"`
+		ServerName         string `yaml:"serverName"`
+		CaFile             string `yaml:"caFile"`
+	} `yaml:"tls"`
+	Layout map[string]interface{} `yaml:"layout"`
 }
 
-func importJsonFromFile(path, projectID, datasetID, tableID string) error {
-	ctx := context.Background()
-	client, err := bigquery.NewClient(ctx, projectID)
-	if err != nil {
-		return fmt.Errorf("bigquery.NewClient: %v", err)
-	}
-	defer client.Close()
+func NewElasticsearch(cfg *ElasticsearchConfig) (*Elasticsearch, error) {
+	var caCert []byte
 
-	f, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	source := bigquery.NewReaderSource(f)
-	source.SourceFormat = bigquery.JSON
-	source.AutoDetect = true // Allow BigQuery to determine schema.
-
-	loader := client.Dataset(datasetID).Table(tableID).LoaderFrom(source)
-
-	log.Info().Msgf("loader.Run...")
-	job, err := loader.Run(ctx)
-	if err != nil {
-		return err
-	}
-	log.Info().Msgf("loader.Wait...")
-	status, err := job.Wait(ctx)
-	if err != nil {
-		return err
-	}
-	log.Info().Msgf("loader done.")
-	if err := status.Err(); err != nil {
-		return err
-	}
-	return nil
-}
-
-// TODO(vsbus): test with a table that has limited permissions
-type BigqueryConfig struct {
-        // BigQuery table config
-        // TODO(vsbus): add validator for BQ configs to be set
-        Project string `yaml:"project"`
-        Dataset string `yaml:"dataset"`
-        Table string `yaml:"table"`
-        // Batching config
-        // TODO(vsbus): set default values
-        BatchSize int `yaml:"batch_size"`
-        MaxRetries int `yaml:"max_retries"`
-        IntervalSeconds int `yaml:"interval_seconds"`
-        TimeoutSeconds int `yaml:"timeout_seconds"`
-}
-
-func NewBigquery(cfg *BigqueryConfig) (*Bigquery, error) {
-	handleBatch := func(ctx context.Context, items []interface{}) []bool {
-		res := make([]bool, len(items))
-		for i := 0; i < len(items); i++ {
-			res[i] = true
+	if len(cfg.TLS.CaFile) > 0 {
+		readFile, err := ioutil.ReadFile(cfg.TLS.CaFile)
+		if err != nil {
+			return nil, err
 		}
-		path := "/tmp/bigquery_batch.json"
-		if err := writeBatchToJsonFile(items, path); err != nil {
-			log.Error().Msgf("Failed to write JSON file: %v", err)
-		}
-		if err := importJsonFromFile(path, cfg.Project, cfg.Dataset, cfg.Table); err != nil {
-			log.Error().Msgf("BigQuery load failed: %v", err)
-		} else {
-			if err := os.Remove(path); err != nil {
-				log.Error().Msgf("Failed to delete file %v: %v", path, err)
-			}
-		}
-		return res
+		caCert = readFile
 	}
 
-	batchWriter := batch.NewWriter(
-		batch.WriterConfig{
-			BatchSize:  cfg.BatchSize,
-			MaxRetries: cfg.MaxRetries,
-			Interval:   time.Duration(cfg.IntervalSeconds) * time.Second,
-			Timeout:    time.Duration(cfg.TimeoutSeconds) * time.Second,
+	tlsClientConfig := &tls.Config{
+		InsecureSkipVerify: cfg.TLS.InsecureSkipVerify,
+		ServerName:         cfg.TLS.ServerName,
+	}
+	tlsClientConfig.RootCAs.AppendCertsFromPEM(caCert)
+
+	client, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: cfg.Hosts,
+		Username:  cfg.Username,
+		Password:  cfg.Password,
+		CloudID:   cfg.CloudID,
+		APIKey:    cfg.APIKey,
+		Transport: &http.Transport{
+			TLSClientConfig: tlsClientConfig,
 		},
-		handleBatch,
-	)
-	batchWriter.Start()
+	})
+	if err != nil {
+		return nil, err
+	}
 
-	return &Bigquery{
-		batchWriter: batchWriter,
+	return &Elasticsearch{
+		client: client,
+		cfg:    cfg,
 	}, nil
 }
 
-type Bigquery struct {
-	batchWriter *batch.Writer
+type Elasticsearch struct {
+	client *elasticsearch.Client
+	cfg    *ElasticsearchConfig
 }
 
-func (e *Bigquery) Send(ctx context.Context, ev *kube.EnhancedEvent) error {
-	e.batchWriter.Submit(ev)
+var regex = regexp.MustCompile(`(?s){(.*)}`)
+
+func formatIndexName(pattern string, when time.Time) string {
+	m := regex.FindAllStringSubmatchIndex(pattern, -1)
+	current := 0
+	var builder strings.Builder
+
+	for i := 0; i < len(m); i++ {
+		pair := m[i]
+
+		builder.WriteString(pattern[current:pair[0]])
+		builder.WriteString(when.Format(pattern[pair[0]+1 : pair[1]-1]))
+		current = pair[1]
+	}
+
+	builder.WriteString(pattern[current:])
+
+	return builder.String()
+}
+
+func (e *Elasticsearch) Send(ctx context.Context, ev *kube.EnhancedEvent) error {
+	var toSend []byte
+
+	if e.cfg.Layout != nil {
+		res, err := convertLayoutTemplate(e.cfg.Layout, ev)
+		if err != nil {
+			return err
+		}
+
+		toSend, err = json.Marshal(res)
+		if err != nil {
+			return err
+		}
+	} else {
+		toSend = ev.ToJSON()
+	}
+
+	var index string
+	if len(e.cfg.IndexFormat) > 0 {
+		now := time.Now()
+		index = formatIndexName(e.cfg.IndexFormat, now)
+	} else {
+		index = e.cfg.Index
+	}
+
+	req := esapi.IndexRequest{
+		Body:  bytes.NewBuffer(toSend),
+		Index: index,
+	}
+
+	if e.cfg.UseEventID {
+		req.DocumentID = string(ev.UID)
+	}
+
+	resp, err := req.Do(ctx, e.client)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	_ = resp.Body
 	return nil
 }
 
-func (e *Bigquery) Close() {
+func (e *Elasticsearch) Close() {
 	// No-op
 }
