5db3ccd0d0083abea05552d003e755b81dc2c3b3
omauger <ophelie.mortain@gmail.com>
2021-08-24 16:09:47 +0200

    Update Kafka output to support sasl auth

--- pkg/sinks/kafka.go
+++ pkg/sinks/kafka.go
@@ -6,6 +6,8 @@ import (
 	"crypto/x509"
 	"encoding/json"
 	"io/ioutil"
+	"math/rand"
+	"time"
 
 	"github.com/Shopify/sarama"
 	"github.com/opsgenie/kubernetes-event-exporter/pkg/kube"
@@ -14,9 +16,15 @@ import (
 
 // KafkaConfig is the Kafka producer configuration
 type KafkaConfig struct {
-	Topic   string                 `yaml:"topic"`
-	Brokers []string               `yaml:"brokers"`
-	Layout  map[string]interface{} `yaml:"layout"`
+	Topic        	 string                 `yaml:"topic"`
+	Brokers      	 []string               `yaml:"brokers"`
+	Layout       	 map[string]interface{} `yaml:"layout"`
+	ClientId     	 string                 `yaml:"clientId"`
+	Version      	 string                 `yaml:"version"`
+	MaxBytesSize 	 int                    `yaml:"maxBytesSize" default:"1000000"`
+	Timeout      	 int32                  `yaml:"timeout" default:"10"`
+	CompressionCodec string					`yaml:"compressionCodec" default:"none"`
+	KeepAlive        int32					`yaml:"keepAlive" default:"0"`
 	TLS     struct {
 		Enable             bool   `yaml:"enable"`
 		CaFile             string `yaml:"caFile"`
@@ -24,12 +32,25 @@ type KafkaConfig struct {
 		KeyFile            string `yaml:"keyFile"`
 		InsecureSkipVerify bool   `yaml:"insecureSkipVerify"`
 	} `yaml:"tls"`
+	SASL struct {
+		Enable    bool   `yaml:"enable"`
+		Username  string `yaml:"username"`
+		Password  string `yaml:"password"`
+	} `yaml:"sasl"`
 }
 
 // KafkaSink is a sink that sends events to a Kafka topic
 type KafkaSink struct {
 	producer sarama.SyncProducer
 	cfg      *KafkaConfig
+}
+
+var CompressionCodecs = map[string]sarama.CompressionCodec{
+	"none": sarama.CompressionNone,
+	"snappy": sarama.CompressionSnappy,
+	"gzip": sarama.CompressionGZIP,
+	"lz4": sarama.CompressionLZ4,
+	"zstd": sarama.CompressionZSTD,
 }
 
 func NewKafkaSink(cfg *KafkaConfig) (Sink, error) {
@@ -88,17 +109,24 @@ func createSaramaProducer(cfg *KafkaConfig) (sarama.SyncProducer, error) {
 	// Default Sarama config
 	saramaConfig := sarama.NewConfig()
 	saramaConfig.Version = sarama.MaxVersion
+	saramaConfig.Metadata.Full = true
+	saramaConfig.ClientID = cfg.ClientId
 
 	// Necessary for SyncProducer
 	saramaConfig.Producer.Return.Successes = true
 	saramaConfig.Producer.Return.Errors = true
+	if _, ok := CompressionCodecs[cfg.CompressionCodec]; ok {
+		saramaConfig.Producer.Compression = CompressionCodecs[cfg.CompressionCodec]
+	}
+	saramaConfig.Producer.MaxMessageBytes = cfg.MaxBytesSize
+	saramaConfig.Producer.Timeout = time.Duration(rand.Int31n(cfg.Timeout)) * time.Second
+
+	// Net Config
+	saramaConfig.Net.KeepAlive = time.Duration(rand.Int31n(cfg.KeepAlive)) * time.Second
 
 	// TLS Client auth override
 	if cfg.TLS.Enable {
-		cert, err := tls.LoadX509KeyPair(cfg.TLS.CertFile, cfg.TLS.KeyFile)
-		if err != nil {
-			return nil, err
-		}
+
 		caCert, err := ioutil.ReadFile(cfg.TLS.CaFile)
 		if err != nil {
 			return nil, err
@@ -109,10 +137,25 @@ func createSaramaProducer(cfg *KafkaConfig) (sarama.SyncProducer, error) {
 
 		saramaConfig.Net.TLS.Enable = true
 		saramaConfig.Net.TLS.Config = &tls.Config{
-			Certificates:       []tls.Certificate{cert},
 			RootCAs:            caCertPool,
 			InsecureSkipVerify: cfg.TLS.InsecureSkipVerify,
 		}
+
+		if cfg.TLS.CertFile != "" && cfg.TLS.KeyFile != "" {
+			cert, err := tls.LoadX509KeyPair(cfg.TLS.CertFile, cfg.TLS.KeyFile)
+			if err != nil {
+				return nil, err
+			}
+
+			saramaConfig.Net.TLS.Config.Certificates = []tls.Certificate{cert}
+		}
+	}
+
+	// SASL Client auth
+	if cfg.SASL.Enable {
+		saramaConfig.Net.SASL.Enable = true
+		saramaConfig.Net.SASL.User = cfg.SASL.Username
+		saramaConfig.Net.SASL.Password = cfg.SASL.Password
 	}
 
 	// TODO: Find a generic way to override all other configs
